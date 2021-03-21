d59350e501014550743641bab544432164af9481
Michellembaitsa <michellembaitsaa@gmail.com>
2021-03-21 21:34:07 +0300

    Arrays

---
+++ src/Arrays.kt
@@ -0,0 +1,31 @@
+import java.util.*
+
+fun main() {
+    name()
+    cities()
+    numbers()
+    var x=(Arrays.toString(myNames("Nancy","Caitlin","Brian")))
+    println(x)
+}
+fun name(){
+    var namesArray=arrayOf("Betty","Zida","Wamani","Nancy")
+    println(Arrays.toString(namesArray))
+}
+fun cities(){
+    var cities= arrayOf("Prague","Venice","Florence","Sydney","Melbourne")
+    var sortedCities=cities.sortedArray()
+    println(Arrays.toString(sortedCities))
+}
+fun numbers(){
+    var numbersArray= arrayOf(32,17,4,213,78,11,158,62)
+    var total= numbersArray[2]+numbersArray[5]
+    println(total)
+    var index=numbersArray.indexOf(158)
+    println(index)
+    var sortedNumbersArray=numbersArray.sortedArray()
+    println(Arrays.toString(sortedNumbersArray))
+}
+fun myNames(a:String,b:String,c:String):Array<String>{
+    var result= arrayOf(a,b,c)
+    return result
+}
